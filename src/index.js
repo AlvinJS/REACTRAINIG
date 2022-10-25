ee3e7120022a86dcffe62c271ce1055e143fff16
AlvinJS <manzi1alvin@gmail.com>
2022-10-25 23:36:24 +0200

    module concept

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Teacher } from "./teacher";
 
 const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById('root'));
 
+
+
+const jobs = [
+    {id:1, isActive: true},
+    {id:2, isActive: false},
+    {id:3, isActive: true},
+];
+console.log (jobs);
+
+
+const activeJobs = jobs.filter(function(job) {return job.isActive;});
+const activeJob = jobs.filter(job => job.isActive);
+
+console.log(activeJobs);
+console.log(activeJob);
+
+const teacher = new Teacher ("Mosh","MSc");
+console.log (teacher);
+teacher.walk();
+teacher.teach();
 
